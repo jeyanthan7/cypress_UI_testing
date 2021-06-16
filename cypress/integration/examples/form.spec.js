eabe4b5080a27c7f7b89fee4237516bf5b1e5325
Jeyapathakaramuthu Chandrasekaran <jeyanthan7@gmail.com>
2021-06-16 23:45:32 +1000

    sample files+readme updated+run sample_file

---
+++ cypress/integration/examples/form.spec.js
@@ -0,0 +1,8 @@
+describe("Form test", () => {
+    it("Can fill the form", () => {
+      cy.visit("/");
+      cy.get("form");
+  
+      cy.get('input[name="name"]').type("Molly");
+    });
+  });
